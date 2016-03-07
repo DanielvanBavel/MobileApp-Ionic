9514eae7c9c3fcad6d511da3952180ffc6be4037
DanielvanBavel <daniel.ajax.1996@hotmail.com>
2016-03-07 09:25:11 +0100

    update

--- musicApp/www/js/controllers.js
+++ musicApp/www/js/controllers.js
@@ -1,4 +1,5 @@
 angular.module('music.controllers', [])
+// angular.module('ionicApp', ['ionic', 'ionic-audio'])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
@@ -81,7 +82,9 @@ angular.module('music.controllers', [])
 	// Simple GET request example:
 	$http({
 		method: 'GET',
-		url: 'https://www.googleapis.com/youtube/v3/playlists&LLN0Ob_Ta6bIUN_lYyKxI_Cg&key=AIzaSyAnoF9yGSWHOEttc0dc_pLoEbZqgMafuLI&part=snippet,contentDetails,statistics,status'
+		url: 'https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=50&playlistId=FLL7gFFBTgbNSpBReiip0xdA&fields=items%2CpageInfo&key=AIzaSyAnoF9yGSWHOEttc0dc_pLoEbZqgMafuLI'
+
+
 		}).then(function successCallback(response) {
 			console.log(response);
 		// this callback will be called asynchronously
@@ -91,7 +94,51 @@ angular.module('music.controllers', [])
 		// called asynchronously if an error occurs
 		// or server returns response with an error status.
 	});
+
+	$response = 
+
+	 $scope.tracks = [
+        {
+            url: '',
+            artist: 'The Police',
+            title: 'Message in a bottle',
+            art: 'https://ionic-audio.s3.amazonaws.com/The_Police_Greatest_Hits.jpg'
+        },
+        {
+            url: 'https://ionic-audio.s3.amazonaws.com/Roxane.mp3',
+            artist: 'The Police',
+            title: 'Roxane',
+            art: 'https://ionic-audio.s3.amazonaws.com/The_Police_Greatest_Hits.jpg'
+        }
+    ];
 })
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+})
+
+
+
+
+
+
+
+
+
+
+// .controller('MyCtrl', function($scope) {
+ 
+//   $scope.tracks = [
+//         {
+//             url: 'https://ionic-audio.s3.amazonaws.com/Message%20in%20a%20bottle.mp3',
+//             artist: 'The Police',
+//             title: 'Message in a bottle',
+//             art: 'https://ionic-audio.s3.amazonaws.com/The_Police_Greatest_Hits.jpg'
+//         },
+//         {
+//             url: 'https://ionic-audio.s3.amazonaws.com/Roxane.mp3',
+//             artist: 'The Police',
+//             title: 'Roxane',
+//             art: 'https://ionic-audio.s3.amazonaws.com/The_Police_Greatest_Hits.jpg'
+//         }
+//     ];
+// });
