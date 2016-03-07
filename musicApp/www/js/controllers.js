7cc0393ed502964bc496e772bf0b2375bddcef44
DanielvanBavel <daniel.ajax.1996@hotmail.com>
2016-03-07 11:56:33 +0100

    controller soundcloud update

--- musicApp/www/js/controllers.js
+++ musicApp/www/js/controllers.js
@@ -1,5 +1,5 @@
 angular.module('music.controllers', [])
-// angular.module('ionicApp', ['ionic', 'ionic-audio'])
+
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
@@ -95,50 +95,17 @@ angular.module('music.controllers', [])
 		// or server returns response with an error status.
 	});
 
-	$response = 
-
-	 $scope.tracks = [
-        {
-            url: '',
-            artist: 'The Police',
-            title: 'Message in a bottle',
-            art: 'https://ionic-audio.s3.amazonaws.com/The_Police_Greatest_Hits.jpg'
-        },
-        {
-            url: 'https://ionic-audio.s3.amazonaws.com/Roxane.mp3',
-            artist: 'The Police',
-            title: 'Roxane',
-            art: 'https://ionic-audio.s3.amazonaws.com/The_Police_Greatest_Hits.jpg'
-        }
-    ];
+	$http({
+		method: 'GET',
+		url: 'https://api.soundcloud.com/users/28914014/favorites?client_id=e72abce51a00fd0b1b9e8f30410cbab8'
+	}).then(function successCallback(response) {
+			console.log(response);
+		}, function errorCallback(response) {
+			console.log('het is kapot:(((((((((');
+		// called asynchronously if an error occurs
+		// or server returns response with an error status.
+	});
 })
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
 })
-
-
-
-
-
-
-
-
-
-
-// .controller('MyCtrl', function($scope) {
- 
-//   $scope.tracks = [
-//         {
-//             url: 'https://ionic-audio.s3.amazonaws.com/Message%20in%20a%20bottle.mp3',
-//             artist: 'The Police',
-//             title: 'Message in a bottle',
-//             art: 'https://ionic-audio.s3.amazonaws.com/The_Police_Greatest_Hits.jpg'
-//         },
-//         {
-//             url: 'https://ionic-audio.s3.amazonaws.com/Roxane.mp3',
-//             artist: 'The Police',
-//             title: 'Roxane',
-//             art: 'https://ionic-audio.s3.amazonaws.com/The_Police_Greatest_Hits.jpg'
-//         }
-//     ];
-// });
